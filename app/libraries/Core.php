392454b7d4b51759a6fa4394a8c903b594b90fc5
Malle Alaru <malle.alaru@khk.ee>
2020-01-17 18:10:25 +0200

    OOP - pvk - app - public

---
+++ app/libraries/Core.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class Core
+{
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
+    public function __construct()
+    {
+        $url = $this->getUrl();
+        // controller
+        if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+        require_once '../app/controllers/'.$this->currentController.'.php';
+        $this->currentController = new $this->currentController;
+        // method
+        if(method_exists($this->currentController, $url[1])){
+            $this->currentMethod = $url[1];
+            unset($url[1]);
+        }
+
+        echo '<pre>';
+        print_r($url);
+        echo '</pre>';
+    }
+
+    public function getUrl(){
+        if(isset($_GET['url'])){
+            $url = $_GET['url'];
+            $url = rtrim($url, '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}
