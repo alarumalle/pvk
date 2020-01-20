851916376d3f84f6bf806bc4db2d574f6d957e73
Malle Alaru <malle.alaru@khk.ee>
2020-01-20 16:23:43 +0200

    panime paika autoloadi pohiklasside kasutamiseks

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 //see bootstrap on meie konfiguratsioonifail, abifail
 //laadime vajalikud raamatukogud (libraries)
-require_once 'libraries/Core.php';
-require_once 'libraries/Controller.php'; // model ja view meetodid saavad sellega k@ttesaadavaks
+//spl a r (), selle asemel et yhekaupa, v6tame kohe kasutusele k6ik libraries?
+spl_autoload_register(function ($className) {
+    require_once 'libraries/'.$className.'.php';
+});
+//require_once 'libraries/Core.php';
+//require_once 'libraries/Controller.php';
+// model ja view meetodid saavad sellega k@ttesaadavaks
