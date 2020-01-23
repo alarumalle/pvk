34a3cb6e4905209c746ed158b17d4ba90988ec24
Malle Alaru <malle.alaru@khk.ee>
2020-01-23 15:41:34 +0200

    ?

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,5 +1,5 @@
 <?php
-
+//see on see fail sellest kuidas ma saan ABga sooritada t88d
 
 class Database
 {
@@ -12,12 +12,59 @@ class Database
     private $dbh;
     private $stmt;
     private $error;
-}
+
 
 // Database constructor
 
 public function __construct()
 {
-    $dsn = ''
-//        ei joua kaasa kirjutada, tekst puudu
+    $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+    $options = array(
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+    try {
+        $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+    } catch (PDOException $e) {
+        $this->error = $e->getMessage();
+        echo $this->error . '<br>';
+    }
+}
+//p@ringu ettevalmistus, prepare element
+public function query(){
+    $this->stmt = $this->dbh->prepare($sql);
+}
+
+//nyyd peame panema v@@rtused ehk v@ljund
+//}
+
+public function bind($param, $value, $type=null){
+    if(is_null($type)){
+//        peab tegema valiku...
+        switch (true){
+            case is_int($value):
+                $type = PDO::PARAM_INT;
+                break;
+            case is_bool($value):
+                $type = PDO::PARAM_BOOL;
+                break;
+            case is_null($value):
+                $type = PDO::PARAM_NULL;
+                break;
+            default:
+                $type = PDO::PARAM_STR;
+        }
+    }
+    $this->stmt->bindValue($param, $value, $type);
+}
+
+public function execute(){
+    $this->stmt->execute();
+}
+
+public function getUsers(){
+    $this->execute();
+    return $this->
+//        ???
+}
 }
