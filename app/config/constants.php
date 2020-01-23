f0aa86a73a50c0c5dcc5987d3769a174b9e0fcb5
Malle Alaru <malle.alaru@khk.ee>
2020-01-23 14:03:38 +0200

    lisatud db parameetrid config faili ja DB klass (poolik)

--- app/config/constants.php
+++ app/config/constants.php
@@ -7,3 +7,9 @@ define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
 //dirname kysib mis kaustas...
 //defineerime konstandi
 define('APPROOT', dirname(dirname(__FILE__)));
+
+//db parameetrid
+define('DB_HOST', 'localhost');
+define('DB_USER', 'user');
+define('DB_PASS', '');
+define('DB_NAME', 'pdo');
---
+++ app/libraries/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Database
+{
+    private $host = DB_HOST;
+//    vead
+    private $user = 'root';
+    private $pass = '';
+    private $dbname ='pdo';
+
+    private $dbh;
+    private $stmt;
+    private $error;
+}
+
+// Database constructor
+
+public function __construct()
+{
+    $dsn = ''
+//        ei joua kaasa kirjutada, tekst puudu
+}
