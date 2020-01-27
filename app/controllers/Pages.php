accea5fa004fea45d91ea101a249b9268820820f
Malle Alaru <malle.alaru@khk.ee>
2020-01-27 13:23:26 +0200

    raamistik on valmis

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -9,23 +9,8 @@ class Pages extends Controller
     public function __construct()
 //model osa 23012020
     {
-        $this->pagesModel = $this->model('Page');
     }
 
-    public function test()
-    {
-        $users = $this->pagesModel->getUsers();
-        print_r($users);
-    }
-
-//23012020 lisatu lopp
-
-//siit edasi view kausta edit vaatesse
-    public function edit(){
-        $this->view('pages/edit');
-    }
-//see aktiveerub kui index oleks meetod (see on default)
-//kuna pages extends controller, siis me vaatame mida view teeb Controller.php klassist!!!
     public function index() {
         $data = array('title' => 'Pages controller is loaded');
         $this->view('pages/index', $data);
